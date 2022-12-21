7040d666ba2c94106f7b7484db001a8f4f4092e8
Dilfer123 <dilfernisha@gmail.com>
2022-12-21 18:11:04 +0530

    asvcjhca

--- src/main/java/org/git1/Git1.java
+++ src/main/java/org/git1/Git1.java
@@ -5,6 +5,9 @@ public class Git1 {
 	private void first() {
 		
 		System.out.println("hello");
+		System.out.println("dilu");
+		System.out.println("nisha");
+		System.out.println("dilfer");
 		
 	}
 	
